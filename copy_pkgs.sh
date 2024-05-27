9ee961be7fd5f583a8ebbdac97f4dc7de44d73b5
Pial Das <pdas2711@proton.me>
2024-05-27 09:17:10 -0400

    Added copying of individual libraries that are user specified.

--- copy_pkgs.sh
+++ copy_pkgs.sh
@@ -1,21 +1,48 @@
 #!/usr/bin/env bash
 
+# No trailing forward slashes
 init_src_dir="/usr/src/initramfs"
-bins=("/sbin/btrfs" "/bin/busybox" "/usr/sbin/dropbear")
+bins=("/sbin/btrfs" "/bin/busybox" "/usr/sbin/dropbear" "/sbin/cryptsetup")
+libs=("libgcc_s.so.1")
 
 
-mkdir -p ${init_src_dir}/{bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys,usr/lib,usr/lib64,usr/sbin}
+mkdir -p ${init_src_dir}/{bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys,usr/lib,usr/lib64,usr/sbin,run/cryptsetup}
 
-libs=()
+parsed_libs=()
 for bin in "${bins[@]}"; do
 	libs_s=$(lddtree -l ${bin})
 	for (( i=1; i<=$(echo "${libs_s}" | wc -l); i++ )); do
 		lib_path=$(echo "${libs_s}" | sed -n ${i}p)
-		libs+=("${lib_path}")
+		parsed_libs+=("${lib_path}")
 	done
 done
 
+for parsed_lib in "${parsed_libs[@]}"; do
+	echo "Copying ${parsed_lib} ..."
+	cp -L "${parsed_lib}" "${init_src_dir}${parsed_lib}"
+done
+
+lib_paths=()
 for lib in "${libs[@]}"; do
-	echo "Copying ${lib} ..."
-	cp -L "${lib}" "${init_src_dir}${lib}"
+	lib_path_coll=$(find /usr/lib/ -name "${lib}")
+	for (( i=1; i<=$(echo "${lib_path_coll}" | wc -l); i++ )); do
+		lib_path=$(echo "${lib_path_coll}" | sed -n ${i}p)
+		lib_paths+=("${lib_path}")
+	done
+done
+for lib in "${libs[@]}"; do
+	lib_path_coll=$(find /usr/lib64/ -name "${lib}")
+	for (( i=1; i<=$(echo "${lib_path_coll}" | wc -l); i++ )); do
+		lib_path=$(echo "${lib_path_coll}" | sed -n ${i}p)
+		lib_paths+=("${lib_path}")
+	done
+done
+
+for s_lib in "${lib_paths[@]}"; do
+	if [[ "${s_lib}" != "" ]]; then
+		lib_path=$(printf %s "${s_lib}" | sed -e 's/\/[^/]*$//g')
+		mkdir -p "${init_src_dir}${lib_path}"
+		echo "Copying ${s_lib} ..."
+		cp "${s_lib}" "${init_src_dir}${lib_path}"
+	fi
 done
